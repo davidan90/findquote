66137f13f82184b8cdaae6b533ee40bd28cddd61
davidan90 <david16an@gmail.com>
2018-02-12 15:52:43 +0100

    add models folder

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,10 +1,3 @@
-// // https://github.com/diegohaz/arc/wiki/Containers
-// const req = require.context('.', false, /^((?!index).)*\.js$/)
-
-// req.keys().forEach((key) => {
-//   const containerName = key.replace(/^\.\/([^.]+)\.js$/, '$1')
-//   module.exports[containerName] = req(key).default
-// })
 const req = require.context('.', true, /\.\/[^/]+\/[^/]+\/index\.js$/)
 
 req.keys().forEach((key) => {
