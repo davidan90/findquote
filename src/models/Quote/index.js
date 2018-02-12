1733fca780347982b3699f599e69c1c9b6b1cf04
davidan90 <david16an@gmail.com>
2018-02-12 16:02:35 +0100

    create Quote class

---
+++ src/models/Quote/index.js
@@ -0,0 +1,31 @@
+class Quote {
+    constructor(quote = null, author = null, category = null) {
+        this._quote = quote
+        this._author = author
+        this._category = category
+    }
+
+    get quote() {
+        return this._quote
+    }
+
+    set quote(quote) {
+        this._quote = quote
+    }
+
+    get author() {
+        return this._author
+    }
+
+    set author(author) {
+        this._author = author
+    }
+
+    get category() {
+        return this._category
+    }
+
+    set category(category) {
+        this._category = category
+    }
+}
