ce0e2ade65a5c417b4860fc90afb0e3405e9fdc7
davidan90 <david16an@gmail.com>
2018-02-16 21:21:19 +0100

    FQManager has an interval and show quote every 10 secs

--- src/components/organisms/FQManager/index.js
+++ src/components/organisms/FQManager/index.js
@@ -1,31 +1,29 @@
 import React from 'react'
-import { object, arrayOf } from 'prop-types'
-import { FQInput, FQContainer, FQQuoteList } from 'components'
+import { object } from 'prop-types'
+import { FQInput, FQContainer, FQQuoteList, FQQuote } from 'components'
 import { FQButton } from 'containers'
 import { I18nSpan } from 'i18n'
 
 const FQManager = (props) => (
     <div>
         <FQContainer>
-            {/* <FQInput
-                type={'text'}
-                placeholder={'Example'}
-                width={'100%'}
-            /> */}
-            <FQButton>
+            {/* <FQButton>
                 <I18nSpan reference={'fq-manager.fq-button.1'} />
-            </FQButton>
+            </FQButton> */}
         </FQContainer>
         <FQContainer>
-            <FQQuoteList
+            {/* <FQQuoteList
                 quotes={props.listQuote}
+            /> */}
+            <FQQuote
+                quote={props.quoteLoaded}
             />
         </FQContainer>
     </div>
 )
 
 FQManager.propTypes = {
-    listQuote: arrayOf(object),
+    quoteLoaded: object,
 }
 
 export default FQManager
--- src/containers/organisms/FQManager/index.js
+++ src/containers/organisms/FQManager/index.js
@@ -1,32 +1,55 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { object, arrayOf } from 'prop-types'
+import { object } from 'prop-types'
 import { FQManager } from 'components'
+import { quotesActions } from 'store/actions'
 
 class FQManagerContainer extends React.Component {
+    static contextTypes = {
+        api: object,
+    }
+
     static propTypes = {
-        quoteList: arrayOf(object),
+        currentQuote: object,
     }
 
     constructor(props) {
         super(props)
     }
 
+    componentWillMount() {
+        setInterval(this.handleInterval, 10000)
+    }
+
+    handleInterval = () => {
+        const { api } = this.context
+        const { loadQuote } = this.props
+
+        api.getRandomQuote()
+            .then( data => {
+                loadQuote(data)
+            })
+    }
+
     render() {
-        const {
-            quoteList
-        } = this.props
+        const { currentQuote } = this.props
 
         return (
             <FQManager
-                listQuote={quoteList}
+                quoteLoaded={currentQuote}
             />
         )
     }
 }
 
 const mapStateToProps = state => ({
-    quoteList: state.quotes.currentList,
+    currentQuote: state.quotes.currentQuote,
 })
 
-export default connect(mapStateToProps)(FQManagerContainer)
+const mapDispatchToProps = dispatch => ({
+    loadQuote: (data) => {
+        dispatch(quotesActions.loadQuote(data))
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(FQManagerContainer)
